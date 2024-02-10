4968caa027eb70b5cd6c81e1a895bbbc18f326b6
Harsh Bansal <harshbansal8705@gmail.com>
2024-02-10 14:22:29 +0530

    Added register api

--- controller/register.js
+++ controller/register.js
@@ -1,16 +1,30 @@
 const { User } = require("../model/User")
-
+const bcrypt = require('bcryptjs')
 exports.register = async (req, res) => {
     try {
-        // const user = new User(req.body);
-        const user = new User({
-            email: "example@gmai.com",
-            name: "example",
-        })
+        console.log(req.body);
+        const user = new User(req.body);
+        try {
+            await user.validate();
+        } catch (e) {
+            return res.status(400).json({ success: false, message: e });
+        }
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+            // Store hash in your password DB.
+            req.body.password = hash;
+            console.log("hashed password", hash);
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ success: false, message: "Error in hashing password" });
+            }
+        });
+        await user.save();
+        console.log("user registered successfully" , user);
         
         res.status(200).json({ success: true, message: "User registered successfully" });
     }
     catch (e) {
-        res.status(400).json({ success: false, message: e.message })
+        console.log(e)
+        res.status(400).json({ code: -1,success: false, message: e.message })
     }
 }
