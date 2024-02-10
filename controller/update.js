4a2b529242a0465ce38e2e25468bcabc89b01502
Harry <harsagra3478@gmail.com>
2024-02-10 14:45:33 +0530

    update import error

--- controller/update.js
+++ controller/update.js
@@ -1,11 +1,11 @@
 const { User } = require('../model/user');
 
-module.exports = async (req, res) => {
+exports.update = async (req, res) => {
     
-    const { name, email ,rollNo, phoneNo ,yearOfPassing , personalEmail , instiEmail , dateOfBirth , department ,emergencyPhoneNo    } = req.body;
+    const { name ,rollNo, phoneNo ,yearOfPassing , personalEmail , instiEmail , dateOfBirth , department ,emergencyPhoneNo    } = req.body;
     
     const user = await User.findOneandUpdate({email:email}, {name, email ,rollNo, phoneNo ,yearOfPassing , personalEmail , instiEmail , dateOfBirth , department ,emergencyPhoneNo    } , {new:true});
     if(!user) return res.status(400).send('User not found');
     user.save();
     res.status(200).send(user);
-    
+}
--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const { loginWithPassword, loginWithOtp } = require("./controller/user")
 const { update } = require("./controller/update")
 const { register } = require("./controller/register")
 const { verifyOTP } = require("./controller/user")
-const { authenticateToken } = require("./middlewares/auth")
+const authenticateToken = require("./middlewares/auth")
+const { upload } = require("./controller/update")
+
 const { client } = require("./redis")
 const app = express();
 require('dotenv').config()
 
 try {
     mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to mongoose"))
-    client.connect("redis://red-cn37f87109ks73eo6930:6379");
+    client.connect("redis://red-cn37f87109ks73eo6930:6379rediss://red-cn37f87109ks73eo6930:hKPRP5WYgHowPIVe4pCz5AHaZ3BIrxmI@singapore-redis.render.com:6379");
     client.on("error", err => console.log("Redis client error: ", err));
     client.on("connect", () => console.log("Connected to redis"));
 } catch (e) {
@@ -27,7 +29,8 @@ app.use(express.urlencoded({ extended: true }))
 app.post("/login/password", loginWithPassword)
 app.post("/login/otp", loginWithOtp)
 app.post("/login/verify", verifyOTP)
-app.put("/update", authenticateToken, update)
+console.log(authenticateToken , update)
+app.put("/update", authenticateToken , update )
 app.post("/register", register)
 app.listen(8000, () => {
     console.log("Listening on port 8000...")
