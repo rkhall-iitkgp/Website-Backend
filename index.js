22584fc1b1e19324d40eb7d34e9f44ba1ad4819d
Harsh Bansal <harshbansal8705@gmail.com>
2024-02-10 14:32:54 +0530

    Updated index.js - Added redis url

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express")
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { loginWithPassword, loginWithOtp } = require("./controller/user")
+const { update } = require("./controller/update")
+const { register } = require("./controller/register")
 const { verifyOTP } = require("./controller/user")
-const { client }  = require("./redis")
+const { authenticateToken } = require("./middlewares/auth")
+const { client } = require("./redis")
 const app = express();
 require('dotenv').config()
 
 try {
     mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to mongoose"))
-    client.connect();
+    client.connect("redis://red-cn37f87109ks73eo6930:6379");
     client.on("error", err => console.log("Redis client error: ", err));
     client.on("connect", () => console.log("Connected to redis"));
 } catch (e) {
@@ -21,11 +24,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post("/loginWithPassword", loginWithPassword)
-app.post("/loginWithOtp", loginWithOtp)
-app.post("/verify", verifyOTP)
-// app.update("/update", up)
-
+app.post("/login/password", loginWithPassword)
+app.post("/login/otp", loginWithOtp)
+app.post("/login/verify", verifyOTP)
+app.put("/update", authenticateToken, update)
+app.post("/register", register)
 app.listen(8000, () => {
     console.log("Listening on port 8000...")
 })
