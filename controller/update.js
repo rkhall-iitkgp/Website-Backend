9e263aeda12838806b24526ad82b724c2e451083
Harsh Bansal <harshbansal8705@gmail.com>
2024-02-10 14:42:41 +0530

    Update api added - Error handling remaining

--- controller/update.js
+++ controller/update.js
@@ -1,11 +1,13 @@
-const { User } = require('../model/user');
+const { User } = require('../model/User');
 
-module.exports = async (req, res) => {
-    
-    const { name, email ,rollNo, phoneNo ,yearOfPassing , personalEmail , instiEmail , dateOfBirth , department ,emergencyPhoneNo    } = req.body;
-    
-    const user = await User.findOneandUpdate({email:email}, {name, email ,rollNo, phoneNo ,yearOfPassing , personalEmail , instiEmail , dateOfBirth , department ,emergencyPhoneNo    } , {new:true});
-    if(!user) return res.status(400).send('User not found');
+exports.update = async (req, res) => {
+
+    const { name, rollNo, phoneNo, yearOfPassing, personalEmail, instiEmail, dateOfBirth, department, emergencyPhoneNo } = req.body;
+
+    const email = req.user.email;
+
+    const user = await User.findOneAndUpdate({ email: email }, { name, rollNo, phoneNo, yearOfPassing, personalEmail, instiEmail, dateOfBirth, department, emergencyPhoneNo }, { new: true });
+    if (!user) return res.status(400).send('User not found');
     user.save();
     res.status(200).send(user);
-    
+}
